313acd880a8d0c50e0147ab54d8e514af6950f16
Jasco <jhjasco@gmail.com>
2025-03-31 21:55:44 -0300

    exports: date and time function;

--- miniserver.js
+++ miniserver.js
@@ -1,7 +1,23 @@
 var http = require('http');
+var dt = require('./datetimemodule.js');
 
 http.createServer(function (req, res) {
     res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.end("Hello World!");
+    res.write("Bem-vindo! Conex&atilde;o bem sucedida.");
+    res.write("<br /><br />");
+    res.write("At this moment, the date and time are: " + dt.myDateTime());
+    res.write("<br /><br />");
+    res.write("Neste exato momento, a data e hora registradas s&atilde;o: " + dt.myDateTime());
+    res.end();
 }).listen(8080); 
 
+/*
+var http = require('http');
+var dt = require('./myfirstmodule');
+
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.write("The date and time are currently: " + dt.myDateTime());
+  res.end();
+}).listen(8080); 
+*/
